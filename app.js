41d781e89a5aca068b89c1891e158d783b85c445
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-02-24 00:17:34 +0100

    Hide list items using setting style

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
 
 
-
-
-
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
 const input =document.querySelector('input.description');
 const p = document.querySelector('p.description');
 const button = document.querySelector('button.description');
+
+toggleList.addEventListener('click', () => {
+    listDiv.style.display = 'none';
+});
 
 button.addEventListener( 'click', () => {
     p.innerHTML= input.value + ';';
